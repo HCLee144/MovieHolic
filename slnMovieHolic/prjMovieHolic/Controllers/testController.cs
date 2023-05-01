40d3c559beda76dd88262c32cbbb88d10ea80af3
Stanley <stanley160783@gmail.com>
2023-05-01 21:20:13 +0800

    新增後台layout (有新增一個測試controller和view) wwwroot底下新增theme(後台layout用)

---
+++ slnMovieHolic/prjMovieHolic/Controllers/testController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjMovieHolic.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Test()
+        {
+            //測試後台layout用
+            return View();
+        }
+    }
+}
