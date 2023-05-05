b8370636049db54ddcb7cc654c4463585e0e50a9
Stanley <stanley160783@gmail.com>
2023-05-05 18:57:27 +0800

    新增場次展示頁面SessionView SessionView增加選擇日期select list

---
+++ slnMovieHolic/prjMovieHolic/Controllers/MovieSessionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjMovieHolic.Models;
+
+namespace prjMovieHolic.Controllers
+{
+    public class MovieSessionController : Controller
+    {
+
+        private MovieContext _db;
+        public MovieSessionController() 
+        {
+            _db = new MovieContext();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ViewSession()
+        {
+
+            return View();
+        }
+    }
+}
