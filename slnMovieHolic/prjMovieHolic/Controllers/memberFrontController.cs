98174f1a1efcf9c4136c211d9cf91ce5741e049e
MandyLinApp <mandyeee12@gmail.com>
2023-05-08 09:02:57 +0800

    會員中心更新

---
+++ slnMovieHolic/prjMovieHolic/Controllers/memberFrontController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjMovieHolic.Models;
+
+namespace prjMovieHolic.Controllers
+{
+    public class memberFrontController : Controller
+    {
+        
+        public IActionResult memberList(int? id)
+        {
+            MovieContext movieContext = new MovieContext();
+            TMember memberData = movieContext.TMembers.FirstOrDefault(t => t.FMemberId == id);
+            return View(memberData);
+        }
+        public IActionResult memberLogin() 
+        {
+            return View();
+        }
+    }
+}
