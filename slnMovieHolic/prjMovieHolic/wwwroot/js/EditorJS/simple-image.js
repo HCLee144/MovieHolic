661d5899e31ef3a2543b5a9000965c7f972fba64
Peter Lee <leepeter1212@gmail.com>
2023-05-09 15:14:11 +0800

    修改 editor

--- slnMovieHolic/prjMovieHolic/wwwroot/js/EditorJS/simple-image.js
+++ slnMovieHolic/prjMovieHolic/wwwroot/js/EditorJS/simple-image.js
@@ -6,31 +6,43 @@
             icon: '<svg width="17" height="15" viewBox="0 0 336 276" xmlns="http://www.w3.org/2000/svg"><path d="M291 150V79c0-19-15-34-34-34H79c-19 0-34 15-34 34v42l67-44 81 72 56-29 42 30zm0 52l-43-30-56 30-81-67-66 39v23c0 19 15 34 34 34h178c17 0 31-13 34-29zM79 0h178c44 0 79 35 79 79v118c0 44-35 79-79 79H79c-44 0-79-35-79-79V79C0 35 35 0 79 0z"/></svg>'
         };
     }
+
     //contstuctor
     constructor({ data }) {
         this.data = data;
         this.wrapper = undefined;
     }
+
     //render
     render() {
         this.wrapper = document.createElement('div');
+        this.wrapper.classList.add('simple-image');
+
+        if (this.data && this.data.url) {
+            this._createImage(this.data.url, this.data.caption);
+            return this.wrapper;
+        }
+
         const input = document.createElement('input');
 
-        this.wrapper.classList.add('simple-image');
-        this.wrapper.appendChild(input);
-
         input.placeholder = 'Paste an image URL...';
-        input.value = this.data && this.data.url ? this.data.url : '';
+        input.addEventListener('paste', (event) => {
+            this._createImage(event.clipboardData.getData('text'));
+        });
+
+        this.wrapper.appendChild(input);
 
         return this.wrapper;
     }
+
     //createImage
-    _createImage(url) {
+    _createImage(url, captionText) {
         const image = document.createElement('img');
-        const caption = document.createElement('input');
+        const caption = document.createElement('div');
 
         image.src = url;
-        caption.placeholder = 'Caption...';
+        caption.contentEditable = true;
+        caption.innerHTML = captionText || '';
 
         this.wrapper.innerHTML = '';
         this.wrapper.appendChild(image);
@@ -39,9 +51,20 @@
 
     // ... save
     save(blockContent) {
+        const image = blockContent.querySelector('img');
+        const caption = blockContent.querySelector('[contenteditable]');
+
         return {
-            url: blockContent.value
+            url: image.src,
+            caption: caption.innerHTML || ''
         }
     }
     // ... validate
+    validate(savedData) {
+        if (!savedData.url.trim()) {
+            return false;
+        }
+
+        return true;
+    }
 }
