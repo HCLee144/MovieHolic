da802ae74449243417461650a124ff36c747dcf9
Peter Lee <leepeter1212@gmail.com>
2023-05-09 15:49:32 +0800

    修改資料庫，重建entityModel

---
+++ slnMovieHolic/prjMovieHolic/Models/TTagList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prjMovieHolic.Models;
+
+public partial class TTagList
+{
+    public int FTagId { get; set; }
+
+    public string FTagText { get; set; } = null!;
+
+    public virtual ICollection<TArticleTag> TArticleTags { get; set; } = new List<TArticleTag>();
+}
