431fc28451ae3147d092474e3372c54e472a7483
Stanley <stanley160783@gmail.com>
2023-05-11 16:42:14 +0800

    更新ViewSession

--- slnMovieHolic/prjMovieHolic/ViewModels/CMovieSessionViewModel.cs
+++ slnMovieHolic/prjMovieHolic/ViewModels/CMovieSessionViewModel.cs
@@ -10,21 +10,22 @@ namespace prjMovieHolic.ViewModels
 
         public string? selectedSessionStartString { get; set; }
 
-        public int? sessionID { get; set; }
-        public string? TheaterName { get; set; }
+        public int? SessionID { get; set; }
+        public int? TheaterID { get; set; }
 
         public DateTime StartTime { get; set; }
 
         public DateTime EndTime { get; set; }
         public bool hasOrder { get; set; }
-        public int MovieId { get; set; }
+        public int MovieID { get; set; }
 
         public string? MovieName { get; set; }
 
+        public string? MovieLength { get; set; }
+
         public string? MoviePosterPath { get; set; }
 
-
-
+        public string? createDate { get; set; }
 
     }
 }
