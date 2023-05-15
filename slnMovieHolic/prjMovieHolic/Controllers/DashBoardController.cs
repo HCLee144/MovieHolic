69a24aebbbe3c2fad3a6a182314cf566e35f1590
Stanley <stanley160783@gmail.com>
2023-05-16 00:29:45 +0800

    更新DashBoard

--- slnMovieHolic/prjMovieHolic/Controllers/DashBoardController.cs
+++ slnMovieHolic/prjMovieHolic/Controllers/DashBoardController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 using prjMovieHolic.Models;
 
 namespace prjMovieHolic.Controllers
@@ -19,13 +20,38 @@ namespace prjMovieHolic.Controllers
 
         public IActionResult getChartDataForMovieIncome()
         {
-            DateTime dateYesterDay = DateTime.Now.AddDays(-1).Date;
+            DateTime dateYesterDay = DateTime.Now.AddDays(0).Date; //昨天的場次，從今日的0時之前
             DateTime dateWeekAgo = DateTime.Now.AddDays(-7).Date;
             var q = _db.TOrders.Include(i => i.FSession).Include(i => i.FSession.FMovie).AsEnumerable()
                 .Where(i => (i.FSession.FStartTime <= dateYesterDay && i.FSession.FStartTime >= dateWeekAgo))
-                .GroupBy(i => i.FSession.FMovie.FNameCht).Select(g=> new {g.Key,group = g});
-            //CDataForMovieIncome datas = new
-            return View();
+                .OrderByDescending(i=>i.FSession.FMovie.FScheduleStart)
+                .GroupBy(i => i.FSession.FMovie.FNameCht).Select(g=> new {g.Key,group = g}).ToList();
+            List<CDataForMovieIncome> chartData = new List<CDataForMovieIncome>();
+            foreach(var group in q)
+            {
+                CDataForMovieIncome movieData = new CDataForMovieIncome();
+                //按照電影分series
+                movieData.name = group.Key;
+                //每部電影按照前一天到前七天計算票房總合
+                movieData.data = new List<IncomeDataPerDay>();
+                foreach(var order in group.group)
+                {
+                    IncomeDataPerDay income=null;
+                    if (!movieData.data.Where(data => data.x == order.FSession.FStartTime.ToString("MM-dd")).Any())
+                    {
+                        income = new IncomeDataPerDay();
+                        income.x = order.FSession.FStartTime.ToString("MM-dd");
+                        income.y = (int)order.FTotalPrice;
+                        movieData.data.Add(income);
+                    }
+                    else
+                    {
+                        movieData.data.Where(data => data.x == order.FSession.FStartTime.ToString("MM-dd")).First().y += (int)order.FTotalPrice;
+                    }
+                }
+                chartData.Add(movieData);
+            }
+            return Json(chartData);
         }
     }
 }
--- slnMovieHolic/prjMovieHolic/Models/CDataForDashBoard.cs
+++ slnMovieHolic/prjMovieHolic/Models/CDataForDashBoard.cs
@@ -8,10 +8,10 @@
         public string? name { get; set; }
         //series:電影名稱
 
-        public List<Data>? data { get; set; }
+        public List<IncomeDataPerDay>? data { get; set; }
     }
 
-    public class Data
+    public class IncomeDataPerDay
     {
         public string x { get; set; }
         //電影收入stack長條圖，X為日期
