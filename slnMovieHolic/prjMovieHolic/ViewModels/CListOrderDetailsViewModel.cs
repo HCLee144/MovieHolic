f6a826c88e1590dd5a6b62cde5e4e2af18b49733
tingting <@6121smile@gmail.com>
2023-05-16 16:29:59 +0800

    修改配餐畫面 新增訂單明細畫面

--- slnMovieHolic/prjMovieHolic/ViewModels/CListOrderDetailsViewModel.cs
+++ slnMovieHolic/prjMovieHolic/ViewModels/CListOrderDetailsViewModel.cs
@@ -4,12 +4,12 @@
     {
         public string selectedMovieName { get; set; }
         public int selectedMovieID { get; set; }
-        public string selecteDate_od { get; set; }
-        public string theaterName_od { get; set; }
-        public string tickets_od { get; set; }
-        public string seats_od { get; set; }
-        public string set_od { get; set; }
-        public string discount_od { get; set; }
+        public string selecteDate { get; set; }
+        public string theaterName { get; set; }
+        public string tickets { get; set; }
+        public List<string> seats { get; set; }
+        public List<string> set { get; set; }
+        public string discount { get; set; }
         public string totalPrice{get;set;}
     }
 }
