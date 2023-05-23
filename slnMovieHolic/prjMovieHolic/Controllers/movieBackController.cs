77dfc65b55a117275d43a567134100fcc1805427
Stanley <stanley160783@gmail.com>
2023-05-23 14:17:05 +0800

    新增MovieView，新增編輯及查詢系統

--- slnMovieHolic/prjMovieHolic/Controllers/movieBackController.cs
+++ slnMovieHolic/prjMovieHolic/Controllers/movieBackController.cs
@@ -18,161 +18,34 @@ namespace prjMovieHolic.Controllers
             _db = context;
         }
 
-        // GET: movieBack
-        public async Task<IActionResult> Index()
-        {
-            var movieContext = _db.TMovies.Include(t => t.FRating).Include(t => t.FSeries);
-            return View(await movieContext.ToListAsync());
-        }
-
-        // GET: movieBack/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _db.TMovies == null)
-            {
-                return NotFound();
-            }
-
-            var tMovie = await _db.TMovies
-                .Include(t => t.FRating)
-                .Include(t => t.FSeries)
-                .FirstOrDefaultAsync(m => m.FId == id);
-            if (tMovie == null)
-            {
-                return NotFound();
-            }
-
-            return View(tMovie);
-        }
-
-        // GET: movieBack/Create
-        public IActionResult Create()
-        {
-            ViewData["FRatingId"] = new SelectList(_db.TRatings, "FId", "FId");
-            ViewData["FSeriesId"] = new SelectList(_db.TSeries, "FId", "FId");
-            return View();
-        }
-
-        // POST: movieBack/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("FId,FSeriesId,FRatingId,FNameCht,FNameEng,FScheduleStart,FScheduleEnd,FShowLength,FInteroduce,FTrailerLink,FPosterPath,FImagePath,FPrice")] TMovie tMovie)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Add(tMovie);
-                await _db.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["FRatingId"] = new SelectList(_db.TRatings, "FId", "FId", tMovie.FRatingId);
-            ViewData["FSeriesId"] = new SelectList(_db.TSeries, "FId", "FId", tMovie.FSeriesId);
-            return View(tMovie);
-        }
-
-        // GET: movieBack/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _db.TMovies == null)
-            {
-                return NotFound();
-            }
-
-            var tMovie = await _db.TMovies.FindAsync(id);
-            if (tMovie == null)
-            {
-                return NotFound();
-            }
-            ViewData["FRatingId"] = new SelectList(_db.TRatings, "FId", "FId", tMovie.FRatingId);
-            ViewData["FSeriesId"] = new SelectList(_db.TSeries, "FId", "FId", tMovie.FSeriesId);
-            return View(tMovie);
-        }
-
-        // POST: movieBack/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("FId,FSeriesId,FRatingId,FNameCht,FNameEng,FScheduleStart,FScheduleEnd,FShowLength,FInteroduce,FTrailerLink,FPosterPath,FImagePath,FPrice")] TMovie tMovie)
-        {
-            if (id != tMovie.FId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _db.Update(tMovie);
-                    await _db.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TMovieExists(tMovie.FId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["FRatingId"] = new SelectList(_db.TRatings, "FId", "FId", tMovie.FRatingId);
-            ViewData["FSeriesId"] = new SelectList(_db.TSeries, "FId", "FId", tMovie.FSeriesId);
-            return View(tMovie);
-        }
-
-        // GET: movieBack/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _db.TMovies == null)
-            {
-                return NotFound();
-            }
-
-            var tMovie = await _db.TMovies
-                .Include(t => t.FRating)
-                .Include(t => t.FSeries)
-                .FirstOrDefaultAsync(m => m.FId == id);
-            if (tMovie == null)
-            {
-                return NotFound();
-            }
-
-            return View(tMovie);
-        }
-
-        // POST: movieBack/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_db.TMovies == null)
-            {
-                return Problem("Entity set 'MovieContext.TMovies'  is null.");
-            }
-            var tMovie = await _db.TMovies.FindAsync(id);
-            if (tMovie != null)
-            {
-                _db.TMovies.Remove(tMovie);
-            }
-            
-            await _db.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool TMovieExists(int id)
-        {
-          return (_db.TMovies?.Any(e => e.FId == id)).GetValueOrDefault();
-        }
-
         public IActionResult MovieView()
         {
             return View();
+        }
+
+        public IActionResult queryMovieByName(string movieName)
+        {
+            var movie = _db.TMovies.Where(m => m.FNameCht == movieName).FirstOrDefault();
+            return Json(movie);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(TMovie edition)
+        {
+            TMovie movie = _db.TMovies.Where(m => m.FId == edition.FId).FirstOrDefault();
+            if(movie != null)
+            {
+                movie.FNameCht= edition.FNameCht;
+                movie.FNameEng= edition.FNameEng;
+                movie.FScheduleStart= edition.FScheduleStart;
+                movie.FScheduleEnd= edition.FScheduleEnd;
+                movie.FShowLength = edition.FShowLength;
+                movie.FInteroduce = edition.FInteroduce;
+                movie.FTrailerLink= edition.FTrailerLink;
+                movie.FPrice = edition.FPrice;
+                _db.SaveChanges();
+            }
+            return RedirectToAction("MovieView");
         }
     }
 }
