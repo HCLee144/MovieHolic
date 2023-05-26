3f8e5636fc7bde8f02b48c34b493a69edc83f0e7
Stanley <stanley160783@gmail.com>
2023-05-26 16:08:36 +0800

    完成signalR即時線上人數功能

---
+++ slnMovieHolic/prjMovieHolic/Hubs/BackHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics.Metrics;
+
+namespace prjMovieHolic.Hubs
+{
+    public class BackHub : Hub
+    {
+        //public async Task updateCounter()
+        //{
+        //    await Clients.All.SendAsync("updateCounter",FrontHub.counter);
+        //}
+
+
+        
+        public long updateCounter() //目前無作用，前台與後台共用同一hub
+        {
+            return FrontHub.counter;
+        }
+
+    }
+}
---
+++ slnMovieHolic/prjMovieHolic/Hubs/FrontHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+
+namespace prjMovieHolic.Hubs
+{
+    public class FrontHub:Hub
+    {
+        public static long counter=0;
+
+
+        public override async Task OnConnectedAsync()
+        {
+            counter = counter + 1;//當用戶進入頁面在線人數加一
+            await Clients.All.SendAsync("updateCounter", counter); //登入刷新人數
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? e)
+        {
+            counter = counter - 1;
+            await Clients.All.SendAsync("updateCounter", counter); 
+            await base.OnDisconnectedAsync(e);
+        }
+
+        public long getCounter()  //後檯登入時取得當下counter
+        {
+            return counter;
+        }
+    }
+}
