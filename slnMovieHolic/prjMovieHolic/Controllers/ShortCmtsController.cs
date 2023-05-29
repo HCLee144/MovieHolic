672c2c148c1fa8215366b267fa93181328db47ff
Peter Lee <leepeter1212@gmail.com>
2023-05-29 13:32:42 +0800

    短評 新增修改刪除 完成

--- slnMovieHolic/prjMovieHolic/Controllers/ShortCmtsController.cs
+++ slnMovieHolic/prjMovieHolic/Controllers/ShortCmtsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using prjMovieHolic.Models;
+using Xunit.Sdk;
 
 namespace prjMovieHolic.Controllers
 {
@@ -93,16 +94,20 @@ namespace prjMovieHolic.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("FCmtid,FMovieId,FMemberId,FTitle,FRate")] TShortCmt tShortCmt)
+        public async Task<IActionResult> Create([Bind("FMovieId,FMemberId,FTitle,FRate")] TShortCmt tShortCmt)
         {
-            if (ModelState.IsValid)
+
+            if (tShortCmt != null)
             {
-                tShortCmt.FCreatedTime= DateTime.Now;
+                if (tShortCmt.FTitle == null)
+                    tShortCmt.FTitle = string.Empty;
+                tShortCmt.FCreatedTime = DateTime.Now;
+                tShortCmt.FVisible = true;
                 _context.Add(tShortCmt);
                 await _context.SaveChangesAsync();
             }
             sessionCheck();
-            return RedirectToAction("movieDetails", "movieFront", new {id=tShortCmt.FMovieId });
+            return RedirectToAction("movieDetails", "movieFront", new { id = tShortCmt.FMovieId });
         }
 
         // GET: ShortCmts/Edit/5
@@ -119,8 +124,8 @@ namespace prjMovieHolic.Controllers
                 return NotFound();
             }
             sessionCheck();
-            ViewData["FMemberId"] = new SelectList(_context.TMembers, "FMemberId", "FMemberId", tShortCmt.FMemberId);
-            ViewData["FMovieId"] = new SelectList(_context.TMovies, "FId", "FId", tShortCmt.FMovieId);
+            ViewData["FMemberId"] = tShortCmt.FMemberId;
+            ViewData["FMovieId"] = tShortCmt.FMovieId;
             ViewData["FMemberName"] = _context.TMembers.Where(t => t.FMemberId == tShortCmt.FMemberId).Select(t => t.FName).FirstOrDefault();
             ViewData["FMovieName"] = _context.TMovies.Where(t => t.FId == tShortCmt.FMovieId).Select(t => t.FNameCht).FirstOrDefault();
             return View(tShortCmt);
@@ -138,10 +143,11 @@ namespace prjMovieHolic.Controllers
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            else
             {
                 try
                 {
+                    tShortCmt.FEditedTime = DateTime.Now;
                     _context.Update(tShortCmt);
                     await _context.SaveChangesAsync();
                 }
@@ -156,11 +162,11 @@ namespace prjMovieHolic.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                sessionCheck();
+                return RedirectToAction("movieDetails", "movieFront", new { id = tShortCmt.FMovieId }); ;
+
             }
-            ViewData["FMemberId"] = new SelectList(_context.TMembers, "FMemberId", "FMemberId", tShortCmt.FMemberId);
-            ViewData["FMovieId"] = new SelectList(_context.TMovies, "FId", "FId", tShortCmt.FMovieId);
-            return View(tShortCmt);
+
         }
 
         // GET: ShortCmts/Delete/5
@@ -199,14 +205,39 @@ namespace prjMovieHolic.Controllers
             }
 
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("movieDetails", "movieFront", new { id = tShortCmt.FMovieId });
+            //return Ok(new { success=true});
         }
+        
+        [HttpPost]
+        public async Task<IActionResult> remove(int id)
+        {
+            if (_context.TShortCmts == null)
+            {
+                return Problem("Entity set 'MovieContext.TShortCmts'  is null.");
+            }
+            var tShortCmt = await _context.TShortCmts.FindAsync(id);
+            if (tShortCmt != null)
+            {
+                var userId = HttpContext.Session.GetInt32(CDictionary.SK_LOGIN_USER);
+                if (tShortCmt.FMemberId != userId)
+                {
+                    return Ok(new {success=false, error="帳號權限不符"});
+                }
+                    _context.TShortCmts.Remove(tShortCmt);
+            }
+
+            await _context.SaveChangesAsync();
+            return Ok(new { success = true });
+
+        }
+
 
         private bool TShortCmtExists(int id)
         {
             return (_context.TShortCmts?.Any(e => e.FCmtid == id)).GetValueOrDefault();
         }
-        
+
         private bool sessionCheck()
         {
             var userId = HttpContext.Session.GetInt32(CDictionary.SK_LOGIN_USER);
